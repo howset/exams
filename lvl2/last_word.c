14be2a6cfdc96bed3b67cec86900e95323b0f4ca
Howard Setyamukti <hsetyamu@c0c7c1.42berlin.de>
2024-03-28 10:43:30 +0100

    Add lvl2

--- lvl2/last_word.c
+++ lvl2/last_word.c
@@ -1,5 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   last_word.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsetyamu <hsetyamu@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/28 10:41:11 by hsetyamu          #+#    #+#             */
+/*   Updated: 2024/03/28 10:41:12 by hsetyamu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
-#include <stdio.h>
 
 int	main(int argc, char *argv[])
 {
@@ -9,26 +20,16 @@ int	main(int argc, char *argv[])
 	if (argc == 2)
 	{
 		while (argv[1][i])
-			i++;
-		printf("first i: %d\n", i);
-		while (argv[1][i] == ' ' || argv[1][i] == '\t')
 		{
-			i--;
-			j = i;
-		}
-		printf("second i: %d\n", i);
-		printf("j: %d\n", i);
-		while (argv[1][i] != ' ' || argv[1][i] != '\t')
-			i--;
-		i++;
-		printf("third i: %d\n", i);
-		while (i != j)
-		//while (argv[1][i])
-		{
-			write(1, &argv[1][i], 1);
+			if (argv[1][i] == ' ' && (argv[1][i + 1] >= 33 && argv[1][i + 1] <= 126))
+				j = i + 1;
 			i++;
 		}
-		printf("last i: %d\n", i);
+		while (argv[1][j] >= 33 && argv[1][j] <= 126)
+		{
+			write(1, &argv[1][j], 1);
+			j++;
+		}
 	}
 	write(1, "\n", 1);
 	return (0);
