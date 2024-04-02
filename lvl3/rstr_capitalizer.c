6f70e209ca3a276a821ba655acf78d2000c380e7
Howard Setyamukti <hsetyamu@c4b5c6.42berlin.de>
2024-04-02 15:46:54 +0200

    Add lvl3

--- lvl3/rstr_capitalizer.c
+++ lvl3/rstr_capitalizer.c
@@ -7,15 +7,12 @@ int	main(int argc, char *argv[])
 
 	while (i < argc)
 	{
-		if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
-				argv[i][j] = argv[i][j] - 32;
-		write(1, &argv[i][j], 1);
 		while (argv[i][j])
 		{
-			if ((argv[i][j] >= 'a' && argv[i][j] <= 'z') && (argv[i][j-1] == 32 || argv[i][j-1] == 9))
+			if ((argv[i][j] >= 'a' && argv[i][j] <= 'z') && (argv[i][j+1] == 32 || argv[i][j+1] == 9 || argv[i][j+1] == 0))
 				argv[i][j] = argv[i][j] - 32;
 			else if (argv[i][j] >= 'A' && argv[i][j] <= 'Z')
-				argv[i][j] = argv[i][j] - 32;
+				argv[i][j] = argv[i][j] + 32;
 			write(1, &argv[i][j], 1);
 			j++;
 		}
