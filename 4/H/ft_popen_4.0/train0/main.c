26b185538590ac8117e9889f18354d57cae5229d
howsetya <howsetya@googlemail.com>
2025-03-21 17:26:45 +0100

    vbc

--- 4/H/ft_popen_4.0/train0/main.c
+++ 4/H/ft_popen_4.0/train0/main.c
@@ -1,64 +1,7 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include "get_next_line.h"
-
-int ft_popen(const char *file, const char *argv[], char type)
-{
-	int fd[2];
-	pid_t pid;
-	int fd_to_return;
-
-	if (type != 'r' && type != 'w')
-		return (-1);
-	if (pipe(fd) == -1)
-		return (-1);
-	pid = fork();
-	if (pid == -1) // Error forking
-	{
-		close(fd[0]);
-		close(fd[1]);
-		return (-1);
-	}
-	else if (pid == 0) // Child process
-	{
-		if (type == 'r')
-		{
-			close(fd[0]); // Child outputs to pipe, so close read end
-			if (dup2(fd[1], STDOUT_FILENO) == -1) // Redirect stdout to write end of pipe
-				exit(EXIT_FAILURE);
-		}
-		else
-		{
-			close(fd[1]); // Child reads from pipe, so close write end
-			if (dup2(fd[0], STDIN_FILENO) == -1) // Redirect stdin to read end of pipe
-				exit(EXIT_FAILURE);
-		}
-		if (type == 'r') // Close the duplicate
-			close(fd[1]);
-		else
-			close(fd[0]);
-		execvp(file, (char *const *)argv); // Execute the command
-		exit(EXIT_FAILURE); // If execvp returns, it means there was an error
-	}
-	else // Parent process
-	{
-		if (type == 'r')
-		{
-			close(fd[1]); // Parent reads from pipe, so close write end
-			fd_to_return = fd[0];
-		}
-		else
-		{
-			close(fd[0]); // Parent writes to pipe, so close read end
-			fd_to_return = fd[1];
-		}
-		return (fd_to_return);
-	}
-}
-
 // GPT generated main to test
 #include <stdio.h>
 #include <fcntl.h>
+#include "get_next_line.h"
 
 // Declare the ft_popen function
 int ft_popen(const char *file, const char *argv[], char type);
