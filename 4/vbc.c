d3ab62d9e05b2fe820d91211166ac1affc0973bf
howsetya <howsetya@googlemail.com>
2025-04-11 13:27:06 +0200

    Last train b4 exam

--- 4/vbc.c
+++ 4/vbc.c
@@ -1,34 +1,34 @@
-int	main (int argc, char *argv[])
+int	main(int ac, char **av)
 {
-	if (argc != 2)
-		return(1);
-	char *input = argv[1];
-	node *tree = parse_expr(input);
+	if (ac != 2)
+		return 1;
+	char *input = av[1];
+	node *tree = pars_expr(input);
 	if (!tree)
 	{
 		destroy_tree(tree);
-		return (1);
+		return 1;
 	}
 	else if (*input)
 	{
-		unexpected(*input);
+		(unexpected(*input));
 		destroy_tree(tree);
-		return(1);
+		return 1;
 	}
 	printf("%d\n", eval_tree(tree));
 }
 
 node *parse_expr(char **s)
 {
-	node	*left;
-	node	*right;
-	node	newnode;
+	node *left;
+	node *right;
+	node newnode;
 	
 	left = parse_term(s);
 	while (accept(s, '+'))
 	{
 		right = parse_term(s);
-		if(!right)
+		if (!right)
 		{
 			destroy_tree(left);
 			return (NULL);
@@ -38,20 +38,20 @@ node *parse_expr(char **s)
 		newnode.r = right;
 		left = new_node(newnode);
 	}
-	return (newnode);
+	return (left);
 }
 
 node *parse_term(char **s)
 {
-	node	*left;
-	node	*right;
-	node	newnode;
+	node *left;
+	node *right;
+	node newnode;
 	
 	left = parse_fact(s);
 	while (accept(s, '*'))
 	{
 		right = parse_fact(s);
-		if(!right)
+		if (!right)
 		{
 			destroy_tree(left);
 			return (NULL);
@@ -61,31 +61,31 @@ node *parse_term(char **s)
 		newnode.r = right;
 		left = new_node(newnode);
 	}
-	return (newnode);
+	return (left);
 }
 
 node *parse_fact(char **s)
 {
-	node	*expr;
+	node *expr
 	
-	if(accept(s, '('))
+	if (accept(s, '('))
 	{
 		expr = parse_expr(s);
-		if(!expect(s, ')'))
+		if (!expect(s, ')'))
 		{
 			destroy_tree(expr);
-			return(NULL);
+			return (NULL);
 		}
-		return(expr);
+		return (expr)
 	}
-	return (parse_numb(s));	
+	return (parse_numb(s));
 }
 
 node *parse_numb(char **s)
 {
-	node	newnode;
-
-	if(isdigit(**s))
+	node newnode;
+	
+	if (isdigit(**s))
 	{
 		newnode.type = VAL;
 		newnode.val = **s - '0';
