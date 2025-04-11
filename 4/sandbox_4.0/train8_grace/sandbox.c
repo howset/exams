e74bd328024cfc9ff26bf1bf76278e27d2fdfbab
howsetya <howsetya@googlemail.com>
2025-04-11 18:08:29 +0200

    add flow on sandbox train8_grace

--- 4/sandbox_4.0/train8_grace/sandbox.c
+++ 4/sandbox_4.0/train8_grace/sandbox.c
@@ -34,30 +34,7 @@ int	sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 	sa.sa_handler = handle_timeout;
 	sigaction(SIGALRM, &sa, NULL);
 	alarm(timeout);
-	if (WIFSIGNALED(status))
-	{
-		int	exit_code = WTERMSIG(status);
-		if (verbose)
-			printf("Bad function: %s)\n", strsignal(exit_code));
-		return (0);
-	}
-	if (WIFEXITED(status))
-	{
-		int	exit_code = WEXITSTATUS(status);
-		if (exit_code)
-		{
-			if (verbose)
-				printf("Bad function: exited with code %d\n", exit_code);
-			return (0);
-		}
-		else
-		{
-			if (verbose)
-				printf("Nice function!\n");
-			return (1);
-		}
-	}
-	if (waitpid(pid, &status, NULL) == -1) // if its a bad ft
+	if (waitpid(pid, &status, 0) == -1) // if its a bad ft
 	{
 		while (1)
 		{
@@ -76,6 +53,29 @@ int	sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 			}
 			else
 				return (-1);
+		}
+	}
+	if (WIFSIGNALED(status))
+	{
+		int	exit_code = WTERMSIG(status);
+		if (verbose)
+			printf("Bad function: %s\n", strsignal(exit_code));
+		return (0);
+	}
+	if (WIFEXITED(status))
+	{
+		int	exit_code = WEXITSTATUS(status);
+		if (exit_code)
+		{
+			if (verbose)
+				printf("Bad function: exited with code %d\n", exit_code);
+			return (0);
+		}
+		else
+		{
+			if (verbose)
+				printf("Nice function!\n");
+			return (1);
 		}
 	}
 	return (-1);
