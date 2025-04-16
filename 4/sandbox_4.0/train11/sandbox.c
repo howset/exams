8b42bbd14fe6864b4bc56caba61b842f6e639cc2
howsetya <howsetya@googlemail.com>
2025-04-16 12:44:27 +0200

    Train train train

---
+++ 4/sandbox_4.0/train11/sandbox.c
@@ -0,0 +1,80 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+#include <stdbool.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int g_flag;
+
+void timeout_handler(int signum)
+{
+	if (signum == SIGALRM)
+		g_flag = 0;
+}
+
+int sandbox(void(*f)(void), unsigned int timeout, bool verbose)
+{
+	struct sigaction sa;
+	int status;
+	pid_t pid;
+	
+	pid = fork();
+	if (pid == -1)
+		return (-1);
+	else if (pid == 0)
+	{
+		f();
+		exit(EXIT_SUCCESS);
+	}
+	sa.sa_handler = timeout_handler;
+	sigaction(SIGALRM, %sa, NULL);
+	alarm(timeout);
+	if (waitpid(pid, &status, 0) == -1)
+	{
+		while (1)
+		{
+			if (errno == EINTR)
+			{
+				if (g_flag)
+				{
+					if (verbose)
+						printf("%u\n", timeout);
+					kill(pid, SIGKILL);
+					if (waitpid(pid, &status, 0))
+						return (-1);
+					return (0);
+				}
+				continue;
+			}
+			else
+				return (-1);
+		}
+	}
+	if (WIFEXITED(status))
+	{
+		int ex_code = WEXITSTATUS(status);
+		if (ex_code == 0)
+		{
+			if (verbose)
+				printf("Nice!\n");
+			return (1);
+		}
+		else
+		{
+			if (verbose)
+				printf("%d\n", ex_code);
+			return (0);
+		}
+	}
+	if (WIFESIGNALED(status))
+	{
+		int sig_code = WTERMSIG(status);
+			printf("%s\n", strsignal(sig_code));
+		return (0);
+	}
+	return (-1);
+}
---
+++ 4/vbc_4.1/train9/vbc.c
@@ -0,0 +1,105 @@
+int main(int c, char *v[])
+{
+	if (c != 2)
+		return (1);
+	char *inp = v[1];
+	node *tree = parse_low(inp);
+	if (!tree)
+	{
+		desstroy_tree(tree);
+		return (1);
+	}
+	else if (*inp)
+	{
+		unexpected(*inp);
+		destroy_tree(tree);
+		return (1);
+	}
+	printf("%d\n", eval_tree(tree));
+	destroy_tree(tree);
+	return (0);
+}
+
+node *parse_low(char **s)
+{
+	node	*left;
+	node	*right;
+	node	n;
+	
+	left = parse_mid(s);
+	while (accept(s, '+'))
+	{
+		right = parse_mid(s);
+		if (!right)
+		{
+			destroy_tree(left);
+			return (NULL);
+		}
+		n.type = ADD;
+		n.l = left;
+		n.r = right;
+		left = new_node(n);
+	}
+	return (left);
+}
+
+node *parse_mid(char **s)
+{
+	node	*left;
+	node	*right;
+	node	n;
+	
+	left = parse_high(s);
+	while (accept(s, '*'))
+	{
+		right = parse_high(s);
+		if (!right)
+		{
+			destroy_tree(left);
+			return (NULL);
+		}
+		n.type = MULTI;
+		n.l = left;
+		n.r = right;
+		left = new_node(n);
+	}
+	return (left);
+}
+
+node *parse_mid(char **s)
+{
+	node	*expr;
+	
+	if (accept(s, '('))
+	{
+		expr = parse_low(s);
+		if (!expect(s, ')'))
+		{
+			destroy_tree(expr);
+			return (NULL);
+		}
+		return (expr);
+	}
+	else
+		return (parse_numb(s));
+}
+
+node *parse_numb(char **s)
+{
+	node	numb;
+	
+	if (isdigit(s))
+	{
+		numb.type = VAL;
+		numb.val = **s - '0';
+		numb.l = NULL;
+		numb.r = NULL;
+		*s++;
+		return (new_node(numb));	
+	}
+	else
+	{
+		unexpected(**s);
+		return(NULL);
+	}
+}
